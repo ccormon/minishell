d4d3c4f7edbdc2c549ca12a2988c01cc24a90b6a
ccormon <camille.cormon@protonmail.com>
2024-03-29 12:01:09 +0100

    amj 29032024

--- src/executing.c
+++ src/executing.c
@@ -6,15 +6,43 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 13:17:50 by ccormon           #+#    #+#             */
-/*   Updated: 2024/03/28 17:37:31 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/03/29 11:51:17 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	handle_multiple_cmd(t_arg *arg, char **envp)
+void	ft_pipe(t_arg *arg, int cmd_no)
 {
 
+}
+
+void	exec_cmd(t_arg *arg, int cmd_no, char **envp)
+{
+
+}
+
+void	wait_childs(t_arg *arg)
+{
+
+}
+
+int	handle_multiple_cmd(t_arg *param, char **envp)
+{
+	t_arg	*arg;
+	int		cmd_no;
+
+	arg = param;
+	cmd_no = 0;
+	while (arg->cmd_list->next)
+	{
+		ft_pipe(arg, cmd_no);
+		exec_cmd(arg, cmd_no, envp);
+		arg->cmd_list = arg->cmd_list->next;
+	}
+	close(arg->pipe_fd[(cmd_no + 1) % 2][1]);
+	close(arg->pipe_fd[cmd_no % 2][0]);
+	wait_childs(param);
 }
 
 void	executing(t_arg *arg, char **envp)

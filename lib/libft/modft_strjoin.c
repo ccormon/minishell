395365c3bfac32dbd1d46776c309487cb942f08b
Stephie <sdemaude@student.42lehavre.fr>
2024-04-08 12:01:53 +0200

    norminette check

--- lib/libft/modft_strjoin.c
+++ lib/libft/modft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   modft_strjoin.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sdemaude <sdemaude@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:21:45 by sdemaude          #+#    #+#             */
-/*   Updated: 2024/04/04 11:00:56 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/04/08 12:01:21 by sdemaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ char	*modft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
+	char	*dest;	
 
 	i = 0;
 	j = 0;
-	char	*dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (s1)
 	{
 		while (s1[i])
